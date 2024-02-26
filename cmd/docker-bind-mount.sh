cd5c51f0b56affaf790a175187f36eea3a19494f
Mzzqq <rezarifa661@gmail.com>
2024-02-26 08:06:17 +0700

    add bind mount command

---
+++ cmd/docker-bind-mount.sh
@@ -0,0 +1,1 @@
+docker container create --name mongo-data --mount "type=bind, source=/home/mzzqq/mongo-data, destination=/data/db" -p 8080:27017 -e MONGO_INITDB_ROOT_USERNAME=reza -e MONGO_INITDB_ROOT_PASSWORD=12345 mongo:latest
